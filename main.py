a31cde2e87e03b29ca95dad846397a65e4e6ef70
Eswartc <77880820+Eswartc@users.noreply.github.com>
2023-09-12 12:42:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 def count_batteries_by_health(present_capacities):
   return {
-    "healthy": list(range(83, 101)),
-    "exchange": list(range(63, 83)),
-    "failed": list(range(63))
+    "healthy": 0,
+    "exchange": 0,
+    "failed": 0
   }
 
 
